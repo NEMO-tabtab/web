1448a5d4b0a0a959b791afbb04a4805f7532611d
choikeunyoung <ought119@gmail.com>
2026-02-19 22:51:06 +0900

    feat : 바코드 스캔 부분 구

---
+++ src/app/barcode/page.tsx
@@ -0,0 +1,9 @@
+import BarcodeScanner from "./barcodeScanner";
+
+export default function BarcodePage() {
+    return (
+        <div className="p-6">
+            <BarcodeScanner />
+        </div>
+    );
+}
