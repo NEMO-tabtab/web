8f37ee95a4596d098c92fa6ab35f2e6ec2ab9f72
최근영 <108659935+choikeunyoung@users.noreply.github.com>
2026-09-13 17:49:47 +0900

    feat: login, signup api (#82)
    * feat: login, signup api
    
    * fix : singup build 부분 수정 진행
    
    * fix : API 주소 env 파일에서 읽어오도록 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,7 +14,7 @@ export default function Login() {
         e.preventDefault();
 
         try {
-            const res = await fetch("http://3.38.247.4:8080/api/user/login", {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user/login`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
